86845477dea06f2b692ec71ef9a017a9c3557bf6
Christopher Wood <christopherwood07@gmail.com>
2016-04-14 08:58:26 -0700

    Updated the test

---
+++ CMakeFiles/pit_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pit_test.dir/pit_test.c.o"
+  "pit_test.pdb"
+  "pit_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pit_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
